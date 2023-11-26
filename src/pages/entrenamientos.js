ec412c5da73ea4e4d841cf2ed0893d4f692326b0
Jesus Ruiz <jesus.ruiz0216@alumnos.udg.mx>
2023-11-26 16:18:03 -0600

    useUser

--- src/pages/entrenamientos.js
+++ src/pages/entrenamientos.js
@@ -34,7 +34,7 @@ function FirstPage({entrenamientos}) {
 //funcion especial de next para ejecutar una logica antes ded que la pantalla sea devuelta al cliente
 export  const getServerSideProps = async (context)  =>{
   const {data: entrenamientos} = await axios.get(
-    "http://localhost:3000/api/entrenamientos"
+    "https://deployed-lsoccer.vercel.app/api/entrenamientos"
     );
   
   return {
