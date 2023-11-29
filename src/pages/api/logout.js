362a52dbe1162a93acd5bfc74c9a561209a1d594
Jesus Ruiz <jesus.ruiz0216@alumnos.udg.mx>
2023-11-28 21:09:53 -0600

    logout

--- src/pages/api/logout.js
+++ src/pages/api/logout.js
@@ -2,5 +2,5 @@ import withSession from "../../../lib/session";
 
 export default withSession(async (req, res) => {
   req.session.destroy();
-  res.redirect('http://localhost:3000/login');
+  res.redirect('https://deployed-lsoccer.vercel.app/login');
 });
