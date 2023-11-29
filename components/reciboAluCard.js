fdb57724ad80d127bb010d18181630edce41d2c0
Jesus Ruiz <jesus.ruiz0216@alumnos.udg.mx>
2023-11-29 09:36:52 -0600

    img

--- components/reciboAluCard.js
+++ components/reciboAluCard.js
@@ -9,7 +9,7 @@ export function ReciboAluCard({rec}) {
         <h3><p id="layoutTxt">Folio N º </p>{rec.pk_recibo}</h3>
         <h3><p id="layoutTxt">Numero de cuenta cliente: </p>{rec.key_cuenta_pago}</h3>
         <h3><p id="layoutTxt">Monto a pagar:  </p>{rec.monto_pagado} $MXN</h3>
-        <img className="h-48 w-48" src={'http://localhost:3000/img/'+rec.imagen_pago}/>
+        <img className="h-48 w-48" src={'https://deployed-lsoccer.vercel.app/img/'+rec.imagen_pago}/>
         <h3><p id="layoutTxt">Fecha de realizacion de factura: </p>{rec.fecha_pago}</h3>
         <h3><p id="layoutTxt">observaciones: </p>{rec.observaciones}</h3>
         <h3><p id="layoutTxt">Validacion (0 / 1): </p>{rec.validacion}</h3>
