ecf0a235d4b36fa6dfd990795daa1994b2d11ebf
Jesus Ruiz <jesus.ruiz0216@alumnos.udg.mx>
2023-11-30 09:53:47 -0600

    solucionando cates

--- src/pages/aluCateInfo.js
+++ src/pages/aluCateInfo.js
@@ -7,16 +7,16 @@ export default function Home({user, cate}) {
         let i = 0;
         while(i < cate.length)
         {
-            if(user.FK_categoria === cate[i].PK_categoria){
+            if(user.fk_categoria === cate[i].pk_categoria){
                 return(
                     <div>
                         <dl className="grid max-w-xl grid-cols-1 gap-x-8 gap-y-10 lg:max-w-none lg:grid-cols-1 lg:gap-y-16 pt-3">
-                            <div key={cate[i].PK_categoria} 
+                            <div key={cate[i].pk_categoria} 
                             className="text-center flex flex-col rounded-lg bg-white shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)] white:bg-neutral-700 lg">       
                                 <div className="flex flex-col justify-start p-20"> 
                                     <div
                                     className="mb-8 text-xl font-medium text-neutral-800 dark:text-black">
-                                        Categoria: {cate[i].PK_categoria} <br />
+                                        Categoria: {cate[i].pk_categoria} <br />
                                     </div>
                                     <div className="mb-5 text-base text-neutral-600 dark:text-black">
                                         <p>Fecha de inicio de categoria: {cate[i].fecha_categoria_ini} </p> 
