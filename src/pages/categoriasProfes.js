7bf0905a5763bd179796e85e8d003f6112bd8e94
Jesus Ruiz <jesus.ruiz0216@alumnos.udg.mx>
2023-11-30 10:39:39 -0600

    solucionando entrenamientos y cates

--- src/pages/categoriasProfes.js
+++ src/pages/categoriasProfes.js
@@ -34,9 +34,7 @@ export default function Home({user, catesProf, categorias}) {
             <div className="grid max-w-xl grid-cols-2 gap-x-8 gap-y-10 lg:max-w-none lg:grid-cols-3 lg:gap-y-16 pt-3">
                 {
                 cates.map((feature) => (
-                    <dl className="grid max-w-xl grid-cols-1 gap-x-8 gap-y-10 lg:max-w-none lg:grid-cols-2 lg:gap-y-16 pt-3">
-                    <div key={feature.pk_categoria} 
-                    className="text-center flex flex-col rounded-lg bg-white shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)] white:bg-neutral-700 lg">       
+                    <div key={feature.pk_categoria}  className="text-center flex flex-col rounded-lg bg-white shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)] white:bg-neutral-700 lg">
                         <div className="flex flex-col justify-start p-6"> 
                             <div
                             className="mb-8 text-xl font-medium text-neutral-800 dark:text-black">
@@ -47,9 +45,8 @@ export default function Home({user, catesProf, categorias}) {
                                 <p>Fecha de termino de categoria: {feature.fecha_categoria_fin} </p>  
                                 <p>Turno de la categoria: {feature.turno} </p>  
                             </div>
-                        </div>
-                    </div>            
-                </dl>        
+                        </div>          
+                </div>        
                 ))
                 }
             </div>
--- src/pages/entrenaProfes.js
+++ src/pages/entrenaProfes.js
@@ -30,12 +30,9 @@ export default function Home({user, catesProf, categorias}) {
     const misCates = (cates) =>{
         return (
             <>
-                 <div>
-            <dl className="grid max-w-xl grid-cols-2 gap-x-8 gap-y-10 lg:max-w-none lg:grid-cols-3 lg:gap-y-16 pt-3">
                 {
                 cates.map((feature) => (
-                    <div key={feature.pk_categoria} className="grid max-w-xl grid-cols-1 gap-x-8 gap-y-10 lg:max-w-none lg:grid-cols-2 lg:gap-y-16 pt-3"> 
-                    className="text-center flex flex-col rounded-lg bg-white shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)] white:bg-neutral-700 lg">       
+                    <div key={feature.pk_categoria} className="text-center flex flex-col rounded-lg bg-white shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)] white:bg-neutral-700 lg">       
                         <div className="flex flex-col justify-start p-6"> 
                             <div
                             className="mb-8 text-xl font-medium text-neutral-800 dark:text-black">
@@ -55,8 +52,6 @@ export default function Home({user, catesProf, categorias}) {
                     </div>        
                 ))
                 }
-            </dl>
-        </div>
             </>
         )
     }     
